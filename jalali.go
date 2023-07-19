5438512a199bbdc4d1286372ac9b64af969150bb
Erfan Momeni <erfammioi@gmail.com>
2023-07-19 19:04:16 +0330

    fix: add some function and fix problems

--- jalali.go
+++ jalali.go
@@ -2,48 +2,138 @@ package jalali
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
-const DayInSecond uint64 = 60 * 60 * 24
-const YearInSecond uint64 = 365 * DayInSecond
+type Lang int
+
+const (
+	English Lang = iota
+	Persian
+
+	DayInSecond  int64 = 60 * 60 * 24
+	YearInSecond int64 = 365 * DayInSecond
+)
+
+var (
+	days = [][]string{
+		{"Friday", "Saturady", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"},
+		{"جمعه", "شنبه", "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنجشنبه"},
+	}
+
+	months = [][]string{
+		{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"},
+		{"فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"},
+	}
+
+	numbers = []string{
+		"0123456789",
+		"۰۱۲۳۴۵۶۷۸۹",
+	}
+)
 
 type jalaliDateTime struct {
-	year   uint64
-	month  uint64
-	day    uint64
-	hour   uint64
-	minute uint64
-	second uint64
+	locale Lang
+	year   int64
+	month  int64
+	day    int64
+	hour   int64
+	minute int64
+	second int64
+}
+
+func New(year int64, month int64, day int64, hour int64, minute int64, second int64) *jalaliDateTime {
+	return &jalaliDateTime{
+		year:   year,
+		month:  month,
+		day:    day,
+		hour:   hour,
+		minute: minute,
+		second: second,
+	}
+}
+
+func (j *jalaliDateTime) SetLocale(lang Lang) {
+	j.locale = lang
+
+	return
 }
 
 func Now() *jalaliDateTime {
-	return convertGregorianToJalali(time.Now())
+	return ConvertGregorianToJalali(time.Now())
 }
 
-func Seconds(t time.Time) uint64 {
-	seconds := uint64(0)
+func secondsInGregorian(t time.Time) int64 {
+	seconds := int64(0)
+
 	s := time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC)
 	for s.Before(t) {
 		u := s
 		s = s.AddDate(1, 0, 0)
-		seconds += uint64(s.Sub(u).Seconds())
+		seconds += int64(s.Sub(u).Seconds())
 	}
+
 	if s.Equal(t) {
 		return seconds
 	}
 
-	seconds -= uint64(s.Sub(t).Seconds())
+	seconds -= int64(s.Sub(t).Seconds())
+
 	return seconds
 }
 
-func convertGregorianToJalali(t time.Time) *jalaliDateTime {
-	return ToJalali(Seconds(t) -
-		Seconds(time.Date(622, 3, 22, 0, 0, 0, 0, time.UTC)) +
+func secondsInJalali(j *jalaliDateTime) int64 {
+	monthDay := []int64{31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29}
+	seconds := int64(0)
+	year := int64(1)
+
+	for year < j.year {
+		seconds += YearInSecond + int64(IsLeapYear(year))*DayInSecond
+		year++
+	}
+
+	for i := 0; i < int(j.month)-1; i++ {
+		seconds += monthDay[i] * DayInSecond
+	}
+
+	seconds += (j.day - 1) * DayInSecond
+	seconds += j.hour*60*60 + j.minute*60 + j.second
+
+	return seconds
+}
+
+func ConvertGregorianToJalali(t time.Time) *jalaliDateTime {
+	return ToJalali(secondsInGregorian(t) -
+		secondsInGregorian(time.Date(622, 3, 22, 0, 0, 0, 0, time.UTC)) +
 		3*60*60 + 30*60)
 }
 
-func ToJalali(jalaliSeconds uint64) *jalaliDateTime {
+func ConvertJalaliToGregorian(j *jalaliDateTime) time.Time {
+	return ToGregoroian(secondsInJalali(j) +
+		secondsInGregorian(time.Date(622, 3, 22, 0, 0, 0, 0, time.UTC)) -
+		3*60*60 - 30*60)
+}
+
+func ToGregoroian(gregorianSeconds int64) time.Time {
+	j := time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	for gregorianSeconds > 0 {
+		s := j.AddDate(1, 0, 0)
+		yearDuration := int64(s.Sub(j).Seconds())
+		if gregorianSeconds >= yearDuration {
+			j = s
+			gregorianSeconds -= yearDuration
+		} else {
+			j = j.Add(time.Second * time.Duration(gregorianSeconds))
+			gregorianSeconds = 0
+		}
+	}
+
+	return j
+}
+
+func ToJalali(jalaliSeconds int64) *jalaliDateTime {
 	j := &jalaliDateTime{
 		year:   1,
 		month:  1,
@@ -54,7 +144,7 @@ func ToJalali(jalaliSeconds uint64) *jalaliDateTime {
 	}
 
 	for jalaliSeconds >= 60 {
-		yearDuration := YearInSecond + uint64(isLeapYear(j.year))*DayInSecond
+		yearDuration := YearInSecond + int64(IsLeapYear(j.year))*DayInSecond
 		if jalaliSeconds >= yearDuration {
 			jalaliSeconds -= yearDuration
 			j.year++
@@ -62,7 +152,7 @@ func ToJalali(jalaliSeconds uint64) *jalaliDateTime {
 			jalaliSeconds -= DayInSecond
 			j.day++
 
-			if ShouldUpdateMonth(j.year, j.month, j.day) {
+			if shouldUpdateMonth(j.year, j.month, j.day) {
 				j.day = 1
 				j.month++
 			}
@@ -85,7 +175,7 @@ func ToJalali(jalaliSeconds uint64) *jalaliDateTime {
 				j.day++
 			}
 
-			if ShouldUpdateMonth(j.year, j.month, j.day) {
+			if shouldUpdateMonth(j.year, j.month, j.day) {
 				j.day = 1
 				j.month++
 			}
@@ -101,7 +191,7 @@ func ToJalali(jalaliSeconds uint64) *jalaliDateTime {
 
 	return j
 }
-func ShouldUpdateMonth(year uint64, month uint64, day uint64) bool {
+func shouldUpdateMonth(year int64, month int64, day int64) bool {
 	switch month {
 	case 1, 2, 3, 4, 5, 6:
 		if day > 31 {
@@ -114,7 +204,7 @@ func ShouldUpdateMonth(year uint64, month uint64, day uint64) bool {
 		}
 		return false
 	case 12:
-		if isLeapYear(year) == 1 {
+		if IsLeapYear(year) == 1 {
 			if day > 30 {
 				return true
 			}
@@ -130,7 +220,7 @@ func ShouldUpdateMonth(year uint64, month uint64, day uint64) bool {
 	return false
 }
 
-func isLeapYear(year uint64) int {
+func IsLeapYear(year int64) int {
 	switch year % 128 {
 	case 0, 4, 8, 12, 16, 20, 29, 33, 37, 41, 45, 49, 53, 62, 66, 70, 74, 78, 82, 86, 95, 99, 103, 107, 111, 115, 124:
 		return 1
@@ -149,30 +239,128 @@ func isLeapYear(year uint64) int {
 	return 0
 }
 
-func (j *jalaliDateTime) Year() uint64 {
+func Yesterday() *jalaliDateTime {
+	return ConvertGregorianToJalali(time.Now().Add(-24 * time.Hour))
+}
+
+func Tomorrow() *jalaliDateTime {
+	return ConvertGregorianToJalali(time.Now().Add(24 * time.Hour))
+}
+
+func (j *jalaliDateTime) Add(t jalaliDateTime) *jalaliDateTime {
+	newDate := ToJalali(secondsInJalali(j) + secondsInJalali(&t))
+	newDate.SetLocale(j.locale)
+
+	return newDate
+}
+
+func (j *jalaliDateTime) Yesterday() *jalaliDateTime {
+	return ConvertGregorianToJalali(ConvertJalaliToGregorian(j).Add(-24 * time.Hour))
+}
+
+func (j *jalaliDateTime) Tomorrow() *jalaliDateTime {
+	return j.Add(jalaliDateTime{0, 0, 0, 1, 0, 0, 0})
+}
+
+func (j *jalaliDateTime) Year() int64 {
 	return j.year
 }
 
-func (j *jalaliDateTime) Month() uint64 {
+func (j *jalaliDateTime) Month() int64 {
 	return j.month
 }
 
-func (j *jalaliDateTime) Day() uint64 {
+func (j *jalaliDateTime) Day() int64 {
 	return j.day
 }
 
-func (j *jalaliDateTime) Hour() uint64 {
+func (j *jalaliDateTime) Hour() int64 {
 	return j.hour
 }
 
-func (j *jalaliDateTime) Minute() uint64 {
+func (j *jalaliDateTime) Minute() int64 {
 	return j.minute
 }
 
-func (j *jalaliDateTime) Second() uint64 {
+func (j *jalaliDateTime) Second() int64 {
 	return j.second
 }
 
+func (j *jalaliDateTime) TimeStamp() int64 {
+	return secondsInJalali(j)
+}
+
+func (j *jalaliDateTime) DayOfYear() int64 {
+	today := secondsInJalali(j)
+	j.month = 1
+	j.day = 1
+	startOfYear := secondsInJalali(j)
+	duration := today - startOfYear
+
+	return duration / DayInSecond
+}
+
+func (j *jalaliDateTime) DayOfMonth() int64 {
+	return j.day
+}
+
+func (j *jalaliDateTime) DayOfWeek() int64 {
+	duration := secondsInJalali(j)
+	duration /= DayInSecond
+
+	return duration % 7
+}
+
+func (j *jalaliDateTime) WeekToString() string {
+	return localizeDay(j.DayOfWeek(), j.locale)
+}
+
+func (j *jalaliDateTime) MonthToString() string {
+	return localizeMonth(j.month-1, j.locale)
+}
+
+func (j *jalaliDateTime) Time() time.Time {
+	return time.Date(int(j.year), time.Month(j.month), int(j.day), int(j.hour), int(j.minute), int(j.second), 0, time.Local)
+}
+
+func localizeDay(n int64, locale Lang) string {
+	if n <= 0 || n >= int64(len(days[locale])) {
+		n = 0
+	}
+
+	return days[locale][n]
+}
+
+func localizeMonth(n int64, locale Lang) string {
+	if n <= 0 || n >= int64(len(months[locale])) {
+		n = 0
+	}
+
+	return months[locale][n]
+}
+
+func localizeNumber(number string, locale Lang) string {
+	answer := make([]rune, len(number))
+	nums := []rune(numbers[locale])
+	for i, _ := range number {
+		j, _ := strconv.Atoi(string(number[i]))
+		answer[i] = nums[j]
+	}
+
+	return string(answer)
+}
+
 func (j *jalaliDateTime) String() string {
-	return fmt.Sprintf("%04d-%02d-%02d %2d:%2d:%2d", j.year, j.month, j.day, j.hour, j.minute, j.second)
+	switch Lang(j.locale) {
+	case Persian:
+		return fmt.Sprintf("%s-%s-%s %s:%s:%s",
+			localizeNumber(fmt.Sprintf("%04d", j.year), j.locale),
+			localizeNumber(fmt.Sprintf("%02d", j.month), j.locale),
+			localizeNumber(fmt.Sprintf("%02d", j.day), j.locale),
+			localizeNumber(fmt.Sprintf("%02d", j.hour), j.locale),
+			localizeNumber(fmt.Sprintf("%02d", j.minute), j.locale),
+			localizeNumber(fmt.Sprintf("%02d", j.second), j.locale))
+	default:
+		return fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d", j.year, j.month, j.day, j.hour, j.minute, j.second)
+	}
 }
--- jalali_test.go
+++ jalali_test.go
@@ -1,11 +1,23 @@
 package jalali_test
 
 import (
-	"fmt"
-	"github.com/erfanmomeniii/jalali"
 	"testing"
+	"time"
+
+	"github.com/erfanmomeniii/jalali"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_Now(t *testing.T) {
-	fmt.Println(jalali.Now().String())
+	j := jalali.New(1402, 4, 28, 9, 0, 0)
+	j.SetLocale(jalali.Persian)
+
+	assert.Equal(t, j.WeekToString(), "چهارشنبه")
+
+	g := jalali.ConvertJalaliToGregorian(j)
+
+	assert.Equal(t, g.Year(), 2023)
+	assert.Equal(t, g.Month(), time.Month(7))
+	assert.Equal(t, g.Day(), 19)
 }
