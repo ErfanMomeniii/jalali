2b57ac75c5a888c8c29903b517440a912b81c53a
Erfan Momeni <erfammioi@gmail.com>
2023-07-19 14:53:23 +0330

    fix: update formula for finding leap year

--- jalali.go
+++ jalali.go
@@ -5,25 +5,23 @@ import (
 	"time"
 )
 
-const DifferenceInSecond uint64 = 19603728000
 const DayInSecond uint64 = 60 * 60 * 24
 const YearInSecond uint64 = DayInSecond * 365
 
 type jalaliDate struct {
-	Year   uint64
-	Month  uint64
-	Day    uint64
-	Hour   uint64
-	Minute uint64
-	Second uint64
+	Year  uint64
+	Month uint64
+	Day   uint64
 }
 
 func Now() *jalaliDate {
-	return convertGregorianToJalali(Seconds(time.Now()))
+	return convertGregorianToJalali(time.Now())
 }
 
 func Seconds(t time.Time) uint64 {
 	seconds := uint64(0)
+	y, m, d := t.Date()
+	t = time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
 	s := time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC)
 	for s.Before(t) {
 		u := s
@@ -33,54 +31,31 @@ func Seconds(t time.Time) uint64 {
 	if s.Equal(t) {
 		return seconds
 	}
+
 	seconds -= uint64(s.Sub(t).Seconds())
 	return seconds
 }
 
-func convertGregorianToJalali(gregorianSeconds uint64) *jalaliDate {
-	return ToJalai(gregorianSeconds - Seconds(time.Date(622, 3, 22, 0, 0, 0, 0, time.UTC)))
+func convertGregorianToJalali(t time.Time) *jalaliDate {
+	return ToJalali(Seconds(t) - Seconds(time.Date(622, 3, 22, 0, 0, 0, 0, time.UTC)))
 }
 
-func ToJalai(jalaliSeconds uint64) *jalaliDate {
+func ToJalali(jalaliSeconds uint64) *jalaliDate {
 	j := &jalaliDate{
-		Year:   1,
-		Month:  1,
-		Day:    1,
-		Hour:   0,
-		Minute: 0,
-		Second: 0,
+		Year:  1,
+		Month: 1,
+		Day:   1,
 	}
 
-	for jalaliSeconds >= 60 {
+	for jalaliSeconds >= DayInSecond {
 		yearDuration := YearInSecond + uint64(isLeapYear(j.Year))*DayInSecond
 
 		if jalaliSeconds >= yearDuration {
 			jalaliSeconds -= yearDuration
 			j.Year++
-		} else if jalaliSeconds >= DayInSecond {
+		} else {
 			jalaliSeconds -= DayInSecond
 			j.Day++
-			if ShouldUpdateMonth(j.Year, j.Month, j.Day) {
-				j.Day = 1
-				j.Month++
-			}
-
-			if j.Month >= 13 {
-				j.Month = 1
-				j.Year++
-			}
-		} else {
-			jalaliSeconds -= 60
-			j.Minute++
-			if j.Minute >= 60 {
-				j.Minute -= 60
-				j.Hour++
-			}
-
-			if j.Hour >= 24 {
-				j.Hour -= 24
-				j.Day++
-			}
 
 			if ShouldUpdateMonth(j.Year, j.Month, j.Day) {
 				j.Day = 1
@@ -93,8 +68,6 @@ func ToJalai(jalaliSeconds uint64) *jalaliDate {
 			}
 		}
 	}
-
-	j.Second = jalaliSeconds
 
 	return j
 }
@@ -128,13 +101,24 @@ func ShouldUpdateMonth(year uint64, month uint64, day uint64) bool {
 }
 
 func isLeapYear(year uint64) int {
-	if year%4 == 0 {
+	switch year % 128 {
+	case 0, 4, 8, 12, 16, 20, 29, 33, 37, 41, 45, 49, 53, 62, 66, 70, 74, 78, 82, 86, 95, 99, 103, 107, 111, 115, 124:
 		return 1
+	case 24, 57, 90, 119:
+		if year > 473 {
+			return 1
+		} else {
+			return 0
+		}
+	case 25, 58, 91, 120:
+		if year <= 473 {
+			return 1
+		}
+		return 0
 	}
-
 	return 0
 }
 
 func (j *jalaliDate) ToString() string {
-	return fmt.Sprintf("%d - %d - %d  %d : %d : %d", j.Year, j.Month, j.Day, j.Hour, j.Minute, j.Second)
+	return fmt.Sprintf("%d - %d - %d", j.Year, j.Month, j.Day)
 }
--- jalali_test.go
+++ jalali_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/erfanmomeniii/jalali"
 	"testing"
-	"time"
 )
 
 func Test_Now(t *testing.T) {
@@ -13,9 +12,4 @@ func Test_Now(t *testing.T) {
 
 func Test_Seconds(t *testing.T) {
 	fmt.Println(jalali.Now())
-	s := jalali.Seconds(time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC))
-	fmt.Println(s)
-
-	s = jalali.Seconds(time.Date(1, 1, 1, 0, 0, 2, 0, time.UTC))
-	fmt.Println(s)
 }
